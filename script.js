2f3d4afc3529b321468f8b2a46ba8697b8190ded
jahnathan x <jahnathanexantus@gmail.com>
2022-03-31 22:05:16 -0400

    created function

--- script.js
+++ script.js
@@ -5,51 +5,7 @@ const numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
-
-
-function userOption() {
-  var message = prompt("How many characters would you like your password to contain")
-  var length = parseInt(message)
-  if (isNaN(length)) {
-    alert("This is not a number, Please type a number. ")
-    
-  }
-  if (length < 8 || length >128) {
-    alert("password needs to be between the number 8 & 128.")
-    userOption()
-  }
-
-  var passLower = confirm("Click ok to conform including lowercase character")
-  
-  if (passLower) {
-
-  }
-  
-  var passWordup = confirm("Click ok to conform including uppercase character")
-
-  if (passWordup) {
-
-  }
-
-  var specialChar = confirm("Click ok to conform including special characters ")
-
-  if (specialChar) {
-
-  }
-
-  var number = confirm("Click ok to conform including numerical characters")
-
-  if (number){
-
-  }
-  }
-
-
-// prompt the user for the password criteria
-// password lenght 8 < 128
-
-
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -64,6 +20,17 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//   function generatePassword(){
-//   userOption()
-//  }
+
+
+function generatePassword() {
+  var firstPrompt = prompt("How many characters would you like your password to contain")
+  console.log(firstPrompt);
+  if (isNaN(firstPrompt)){
+    alert("Please enter a valid number between 8 & 32.")
+    generatePassword()
+  }
+  if (firstPrompt <8 || firstPrompt > 128){
+    alert("please enter a valid number between 8 and 128. ")
+  }
+
+};
